02c94eb9293d6c442dea70c1e8d2ecc433a4c586
Moe-Janneh <84420142+Moe-Janneh@users.noreply.github.com>
2022-09-27 00:34:22 -0400

    Add files via upload

---
+++ characterdesign.js
@@ -0,0 +1,24 @@
+//character design js
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['jackson.jpg', 'celique.jpg', 'kuurtz.jpg', 'lucius.jpg', 'rominas.jpg'];
+var i = 0;
+
+function prev()
+{
+	if(i <= 0) i =images.length;
+	i--;
+	return setImg();
+}
+
+function next()
+{
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();
+}
+
+function setImg()
+{
+	return slider_img.setAttribute('src', 'images/Character Design/' + images[i]);
+}
